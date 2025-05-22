a2069fa288b7c6a696239924c645d5298206f0d7
Rajan Singh <rkumarsingh@wfscorp.com>
2025-05-22 13:16:28 -0400

    update the clientid asper repo auth0 app

--- config_old.py
+++ config_old.py
@@ -11,7 +11,7 @@ SNOWFLAKE_CONNECTION = {
 
 # Auth0 Configuration
 AUTH0_CONFIG = {
-    "clientId": "11EIyyba4ieIlQFycP1Sc3lJfgqHVMFD",
+    "clientId": "wscvwhAWpFNKEn9VOwA0PVNhFz6o5uEJ",
     "domain": "dev-wfs.auth0.com",
     "redirect_uri": "https://poseidon.dev.aws.wfscorp.com/"
 }
