3d0ea968f6dde9f775c5bdb191b437b1ebeceb9d
Rajan Singh <rkumarsingh@wfscorp.com>
2025-05-29 16:18:40 -0400

    secrets_manager_change

--- config.py
+++ config.py
@@ -1,13 +1,26 @@
 # Snowflake connection parameters
 # DO NOT commit this file to version control
 
-SNOWFLAKE_CONNECTION = {
-    "account": "wfs.us-east-1",
-    "user": "SVCCDP_MCA@WFSCORP.COM",     # Replace with actual username
-    "password": "Marine_MCA2025"  # Replace with actual password
-} 
+import boto3
+import json
+import os
 
+def get_secret(secret_name):
+    region_name = os.getenv("AWS_REGION", "us-east-1")
 
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(service_name='secretsmanager', region_name=region_name)
+
+    try:
+        get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+        secret = get_secret_value_response['SecretString']
+        return json.loads(secret)
+    except Exception as e:
+        raise Exception(f"Unable to retrieve secret: {e}")
+
+# Fetch the secret
+SNOWFLAKE_CONNECTION = get_secret("poseidon_secret_json")
 
 # Auth0 Configuration
 AUTH0_CONFIG = {
